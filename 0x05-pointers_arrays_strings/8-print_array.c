ee6a9ef1f82e3dea4be37d5765e40368db14c1cc
bradleyG14 <mateobowles32@gmail.com>
2022-12-19 18:47:10 +0300

    not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- *: print_array - print `n` elements of an array of integers
+ ** print_array - print `n` elements of an array of integers
  ** @a: int type array pointer
  ** @n: int type integer
  ** Description: Numbers must be separated by comma and space.
@@ -10,11 +10,13 @@
  ** You can only use _putchar to print.
  **/
 
-void print_array(int *a, int n)
+
+void print_array(int *a, int n);
 {
 int i;
 i = 0;
 for (n--; n >= 0; n--, i++)
+;
 {
 printf("%d", a[i]);
 if (n > 0)
